65febe814db8d2a7a833c6ba1d913a4d4e8cb039
Jessica <fr_jess@icloud.com>
2023-01-26 15:52:26 +0100

    [add] type project

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,43 @@
 import axios from 'axios';
 
 export default {
-    name: 'App',
-    data(){
-        return {
-            baseUrl: 'http://127.0.0.1:8000/api/',
-            projects : [],
-            contentMaxLenght: 150,
-            pagination:{
-                current: 1,
-                last: null
-            }
-        }
-    },
-
-    methods:{
-        getApi(){
-            axios.get(this.baseUrl + 'projects', {
-                params:{
-                    page: this.pagination.current
-                }
-            })
-                .then(result => {
-                    this.projects = result.data.projects.data;
-                    console.log(this.projects)
-                    this.pagination.current = result.data.projects.current_page;
-                    this.pagination.last = result.data.projects.last_page;
-                })
-        },
-        truncateText(summary){
-            if(summary.length > this.contentMaxLenght){
-                return summary.substr(0, this.contentMaxLenght) + '...';
-            }
-            return summary;
-        }
-    },
-    mounted(){
-        this.getApi(1);
+  name: 'App',
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects : [],
+      contentMaxLenght: 100,
+      pagination:{
+          current: 1,
+          last: null
+      }
     }
+  },
+
+  methods:{
+    getApi(page){
+      this.pagination.current = page;
+      axios.get(this.baseUrl + 'projects', {
+        params:{
+            page: this.pagination.current
+        }
+      })
+      .then(result => {
+          this.projects = result.data.projects.data;
+          this.pagination.current = result.data.projects.current_page;
+          this.pagination.last = result.data.projects.last_page;
+      })
+    },
+    truncateText(summary){
+      if(summary.length > this.contentMaxLenght){
+        return summary.substr(0, this.contentMaxLenght) + '...';
+      }
+      return summary;
+    }
+  },
+  mounted(){
+    this.getApi(1);
+  }
 }
 
 </script>
@@ -49,10 +49,40 @@ export default {
   <h1>Projects list</h1>
 
   <div>
-    <div v-for="project in projects" :key="project.name" class="project-box">
+    <div v-for="project in projects" :key="project.id" class="project-box">
       <h3>{{project.name}}</h3>
+      <h4>Type: {{ project.type.name }}</h4>
       <p v-html="truncateText(project.summary)"></p>
     </div> 
+  </div>
+
+  <div class="paginator">
+    <button
+    :disabled="pagination.current === 1"
+    @click="getApi(1)"
+    >| &lt; </button>
+
+    <button
+    :disabled="pagination.current === 1"
+    @click="getApi(pagination.current - 1)"
+    >&larr;</button>
+
+    <button
+    v-for="i in pagination.last" :key="i"
+    :disabled="pagination.current === i"
+    @onclick="getApi(i)"
+    >{{ i }}
+    </button>
+
+    <button
+    :disabled="pagination.current === pagination.last"
+    @click="getApi(pagination.current + 1)"
+    >&rarr;</button>
+
+    <button
+    :disabled="pagination.current === pagination.last"
+    @click="getApi(pagination.last)"
+    > >|</button>
   </div>
 
 </div>
